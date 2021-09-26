ade907795aef98586562dce82570bfafc513be3f
Aquila <calchoi028@gmail.com>
2021-09-26 16:21:29 -0600

    feat: added projectiles on click

--- Revenant Beats/js/rpg_objects.js
+++ Revenant Beats/js/rpg_objects.js
@@ -39,9 +39,8 @@ Game_Temp.prototype.reservedCommonEvent = function() {
 };
 
 Game_Temp.prototype.setDestination = function(x, y) {
-    return;
-    // this._destinationX = x;
-    // this._destinationY = y;
+    this._destinationX = x;
+    this._destinationY = y;
 };
 
 Game_Temp.prototype.clearDestination = function() {
